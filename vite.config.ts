c9ddcea03d99fff5b121f9a82e2f7b121c0a8f12
Elvis Liao <llwxi@qq.com>
2025-07-27 21:03:14 +0800

    chore: fix _plugin-vue_export-helper 404
    https://israynotarray.com/other/20220928/1377668819/

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
@@ -9,6 +13,22 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name)
+          const driveLetter = match ? match[0] : ''
+
+          return (
+            driveLetter
+            + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+          )
+        },
+      },
     },
   },
 })
